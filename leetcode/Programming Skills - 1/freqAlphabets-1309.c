4b4d0572c49f1dff43d5533754dc0b3b99843543
bashtag <btstsiy@hotmail.com>
2022-04-27 01:13:52 +0300

    freq alphabets question uploaded

--- leetcode/Programming Skills - 1/freqAlphabets-1309.c
+++ leetcode/Programming Skills - 1/freqAlphabets-1309.c
@@ -1,40 +1,25 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 
 char * freqAlphabets(char * s){
-    printf("strlen -> %d\n", strlen(s));
     char *result = (char *)calloc(strlen(s) + 1, sizeof(char));
+    char letter;
     
     int j = 0;
     for (int i = 0; s[i]; i++, j++)
     {
-        char letter = 0;
-        printf("i = %d, j = %d\n", i, j);
-        if (i < strlen(s) - 2 && s[i + 2] == '#')
+        letter = 0;
+        if (i + 2 < strlen(s) && s[i + 2] == '#')
         {
             letter += (s[i] - 48) * 10 + (s[i + 1] - 48);
             i += 2;
         }
         else
         {
-            printf("%d\n", s[i]);
             letter += s[i] - 48;
         }
-        printf("alo\n");
         result[j] = 'a' + letter - 1;
-        printf("%c\n", result[j]);
     }
     result[j] = '\0';
     
     return (result);
-}
-
-int	main(void)
-{
-	char *str = freqAlphabets("1");
-
-	printf("%s\n", str);
-	free(str);
 }
